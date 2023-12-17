1dde3aa56308222d77c011578cf54f78b3e7f909
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-17 10:12:44 +0000

    shell

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@ int main(void)
 	int pid, status;
 	char *lineptr = NULL;
 	size_t n;
-	char *argv[] = {lineptr, NULL};
+	char *argv[2];
 	char *env[] = {NULL};
 
 	printf("Simple Shell# ");
@@ -23,6 +23,7 @@ int main(void)
 		getline(&lineptr, &n, stdin);
 		lineptr[strlen(lineptr) - 1] = '\0';
 		argv[0] = lineptr;
+		argv[1] = NULL;
 		execve(lineptr, argv, env);
 	}
 	else
