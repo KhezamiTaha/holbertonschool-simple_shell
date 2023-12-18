ce284b28b803ed965ca35c4f18564c7a171184ec
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-18 12:13:10 +0100

    shell.c

--- shell.c
+++ shell.c
@@ -2,8 +2,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <stdlib.h>
-
+/**
+ * main - execve example
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int pid, status;
@@ -11,46 +14,24 @@ int main(void)
 	size_t n;
 	char *argv[2];
 	char *env[] = {NULL};
-	int argc = 0;
-	char *token;
 	while (1)
 	{
-		printf("Simple Shell# ");
-		fflush(stdout);
-
+	printf("Simple Shell# ");
+	fflush(stdout);
+	pid = fork();
+	if (pid == 0)
+	{
 		getline(&lineptr, &n, stdin);
 		lineptr[strlen(lineptr) - 1] = '\0';
-
-		pid = fork();
-
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-
-			token = strtok(lineptr, " ");
-			while (token)
-			{
-				argv[argc++] = token;
-				token = strtok(NULL, " ");
-			}
-
-			execve(argv[0], argv, env);
-
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-
-			wait(&status);
-		}
+		argv[0] = lineptr;
+		argv[1] = NULL;
+		execve(lineptr, argv, env);
 	}
-
-	free(lineptr);
-
-	return 0;
+	else
+	{
+		wait(&status);
+	}
+	}
+	
+	return (0);
 }
