d4e93c31fd3796fd2a3c2b7e8b89fd88331deb00
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-18 20:59:43 +0100

    modification to accept env.c and exit.c

--- main.c
+++ main.c
@@ -1,15 +1,36 @@
 #include "sh.h"
 
-int main(void) {
+int main(void)
+{
     char *lineptr;
-    while (1) {
+    while (1)
+    {
         printf("$ ");
         fflush(stdout);
 
         lineptr = my_getline();
-        execute(lineptr, environ);
+
+        if (strcmp(lineptr, "env") == 0)
+        {
+            print_env();
+        }
+        else
+        {
+            execute(lineptr, environ);
+        }
+
+        if (strcmp(lineptr, "exit") == 0)
+        {
+            exit_shell();
+        }
+        else
+        {
+
+            execute(lineptr, environ);
+        }
+
         free(lineptr);
     }
 
-    return 0;
+    return (0);
 }
