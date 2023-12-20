9347b8e6d012fdbf814c7e5cfc35c8366fc912bb
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-20 11:53:36 +0100

    get_line

---
+++ getline.c
@@ -0,0 +1,27 @@
+#include "sh.h"
+/**
+ * my_getline - Reads a line of input from the standard input.
+ *
+ * Return: A dynamically allocated string containing the entered line.
+ *         Exits the program with an error message if getline fails.
+ */
+char *my_getline(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    nread = getline(&line, &len, stdin);
+    if (nread == -1)
+    {
+        if (isatty(STDIN_FILENO))
+            perror("getline");
+        free(line);
+        exit(EXIT_FAILURE);
+    }
+    if (nread > 0 && line[nread - 1] == '\n')
+    {
+        line[nread - 1] = '\0';
+    }
+    return (line);
+}
