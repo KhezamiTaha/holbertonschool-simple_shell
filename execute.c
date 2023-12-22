543339ef799da944ee9fb0de338133c3503e2746
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-22 19:24:23 +0100

    betty style

--- execute.c
+++ execute.c
@@ -12,18 +12,11 @@ int execute(char *command)
 	int status, n;
 	char *token, *full_path;
 	char **args;
-	
 	char *_env[] = {NULL};
 	int i = 0;
 	struct stat st;
 
 	args = malloc(sizeof(char *) * 1024);
-	if (args == NULL)
-	{
-		perror("malloc array");
-		return (1);
-	}
-
 	token = strtok(command, " ");
 	while (token != NULL)
 	{
@@ -33,17 +26,9 @@ int execute(char *command)
 		i++;
 	}
 	args[i] = NULL;
-
 	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("fork");
-		freeArray(args);
-		return (-1);
-	}
 	if (child_pid == 0)
 	{
-
 		if (stat(args[0], &st) == 0)
 		{
 			execve(args[0], args, _env);
@@ -53,17 +38,6 @@ int execute(char *command)
 			full_path = find_path(args[0]);
 			if (full_path != NULL)
 				args[0] = full_path;
-			else
-			{
-				freeArray(args);
-				return (-1);
-			}
-			n = execve(full_path, args, _env);
-			if (n == -1)
-			{
-				freeArray(args);
-				return (-1);
-			}
 		}
 	}
 	else
@@ -71,6 +45,5 @@ int execute(char *command)
 		wait(&status);
 		freeArray(args);
 	}
-
 	return (0);
 }
--- main.c
+++ main.c
@@ -5,12 +5,10 @@
  * Return: Always returns 0.
  *         Exits the program when the user enters "exit".
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-
 	int n, counter = 0, flag = 0;
 	char *lineptr, *first_argument, *line_no_space;
-	(void)argc;
 
 	while (1)
 	{
@@ -22,55 +20,26 @@ int main(int argc, char *argv[])
 		{
 			if (isatty(STDIN_FILENO))
 				printf("\n");
-			if (flag != 0)
-				return (127);
 			return (0);
 		}
-		if (*lineptr == ' ')
+		line_no_space = handle_spaces(lineptr);
+		if (line_no_space == NULL)
 		{
-			line_no_space = handle_spaces(lineptr);
-			if (line_no_space == NULL)
-			{
-				free(lineptr);
-				continue;
-			}
+			free(lineptr);
+			continue;
 		}
-		else
-			line_no_space = lineptr;
-
-		first_argument = get_first_argument(line_no_space);
-
-		if (strcmp(line_no_space, "env") == 0)
-		{
-			print_env();
-		}
-
-		else if (strcmp(line_no_space, "exit") == 0)
-		{
-			free(first_argument);
-			free(line_no_space);
-			if (flag != 0)
-			{
-				return (127);
-			
-			}
-				return (127);
-			exit(0);
-		}
-
-		else
-		{
-			n = execute(line_no_space);
-			if (n == -1)
-			{	flag = 1;
-				fprintf(stderr, "%s: %d: %s: not found\n", argv[0], counter, first_argument);
-				free(first_argument);
-				free(lineptr);
-				return (0);
-			}
-		}
-		free(first_argument);
-		free(line_no_space);
 	}
+
+	n = execute(line_no_space);
+	if (n == -1)
+	{
+		flag = 1;
+		free(first_argument);
+		free(lineptr);
+		return (0);
+	}
+	free(first_argument);
+	free(line_no_space);
+
 	return (0);
 }
