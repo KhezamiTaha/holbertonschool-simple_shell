92bc65cbefd7b52d77791a00ed15c4490dbe4a23
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-22 12:00:18 +0000

    format

--- handle_spaces.c
+++ handle_spaces.c
@@ -10,20 +10,20 @@ char *handle_spaces(char *lineptr)
 {
 	int length, i, j;
 
-    length = strlen(lineptr);
+	length = strlen(lineptr);
 
-    for (i = 0; i < length && lineptr[i] == ' '; i++)
-    {
-    }
+	for (i = 0; i < length && lineptr[i] == ' '; i++)
+	{
+	}
 
-    if (lineptr[i] == '\0')
-        return (NULL);
+	if (lineptr[i] == '\0')
+		return (NULL);
 
-    for (j = 0; i < length; i++, j++)
-    {
-        lineptr[j] = lineptr[i];
-    }
+	for (j = 0; i < length; i++, j++)
+	{
+		lineptr[j] = lineptr[i];
+	}
 
-    lineptr[j] = '\0';
-    return (lineptr);
+	lineptr[j] = '\0';
+	return (lineptr);
 }
