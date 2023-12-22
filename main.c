36ec097c4f27d712eb511466abfc5012472246b5
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-22 19:36:31 +0100

    last commit

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
  * Return: Always returns 0.
  *         Exits the program when the user enters "exit".
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+
 	int n, counter = 0, flag = 0;
 	char *lineptr, *first_argument, *line_no_space;
+	(void)argc;
 
 	while (1)
 	{
@@ -20,26 +22,55 @@ int main(void)
 		{
 			if (isatty(STDIN_FILENO))
 				printf("\n");
+			if (flag != 0)
+				return (127);
 			return (0);
 		}
-		line_no_space = handle_spaces(lineptr);
-		if (line_no_space == NULL)
+		if (*lineptr == ' ')
 		{
-			free(lineptr);
-			continue;
+			line_no_space = handle_spaces(lineptr);
+			if (line_no_space == NULL)
+			{
+				free(lineptr);
+				continue;
+			}
 		}
-	}
+		else
+			line_no_space = lineptr;
 
-	n = execute(line_no_space);
-	if (n == -1)
-	{
-		flag = 1;
+		first_argument = get_first_argument(line_no_space);
+
+		if (strcmp(line_no_space, "env") == 0)
+		{
+			print_env();
+		}
+
+		else if (strcmp(line_no_space, "exit") == 0)
+		{
+			free(first_argument);
+			free(line_no_space);
+			if (flag != 0)
+			{
+				return (127);
+			
+			}
+				return (127);
+			exit(0);
+		}
+
+		else
+		{
+			n = execute(line_no_space);
+			if (n == -1)
+			{	flag = 1;
+				fprintf(stderr, "%s: %d: %s: not found\n", argv[0], counter, first_argument);
+				free(first_argument);
+				free(lineptr);
+				return (0);
+			}
+		}
 		free(first_argument);
-		free(lineptr);
-		return (0);
+		free(line_no_space);
 	}
-	free(first_argument);
-	free(line_no_space);
-
 	return (0);
 }
